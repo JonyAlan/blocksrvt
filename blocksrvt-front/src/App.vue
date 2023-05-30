ef09b0dcb9e2388ae9baee09fcd9a2dd18b6697a
JonyAlan <jony.nunes@fatec.sp.gov.br>
2023-05-30 11:11:09 -0300

    init

---
+++ blocksrvt-front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+  
+ 
+</style>
